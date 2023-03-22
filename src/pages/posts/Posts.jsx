ac99267e5471b871741494b1b3ea11c41f2ba80c
Rohit w3dev <rohit.k@w3dev.email>
2023-03-22 21:32:47 +0530

    adding comments for each tag used

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -4,19 +4,24 @@ import "./posts.css";
 const Posts = ({ posts }) => {
   console.log(posts);
   return (
-    //main for controlling how the page layout will look
+    //<main> for controlling how the page layout will look
     <main className="post">
       {posts.map((post) => (
-        //div for controlling the look of each post
+        //<div> for controlling the look of each post
         <div className="post-container" key={post.id}>
+          {/*<h2> render only post title */}
           <h2>{post.title} </h2>
+          {/* <p> render only post body */}
           <p>{post.body}</p>
+          {/* <p> render post reactions */}
           <p>Reactions: {post.reactions}</p>
-          {/* div for controlling the look of tags */}
+          {/* <div> for controlling the look of tags */}
           <div className="post-tags">
+            {/* <p> for specifying that the values in front of it are tags  */}
             <p>Tags: </p>
+            {/* <span> for rendering tags values*/}
             {post.tags &&
-              post.tags.map((item, index) => <span key={index}>{item}</span>)}
+              post.tags.map((tag, index) => <span key={index}>{tag}</span>)}
           </div>
         </div>
       ))}
